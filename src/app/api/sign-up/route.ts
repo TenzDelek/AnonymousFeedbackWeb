647b496640a07bc4c30eae01f70acfbb462186cb
TenzDelek <tibetdelek@gmail.com>
2024-05-04 15:40:59 +0530

    final logic

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -29,6 +29,26 @@ export async function POST(request: Request) {
     const verifycode = Math.floor(100000 + Math.random() * 900000).toString();
 
     if (existinguserbyemail) {
+      if(existinguserbyemail.isVerified)
+        {
+          return Response.json(
+            {
+              success: false,
+              message: "User already exist with this email",
+            },
+            {
+              status: 500,
+            }
+          );
+        }
+        else{
+          const hashedpassword= await bcrypt.hash(password,10)
+          //change password only
+          existinguserbyemail.password=hashedpassword;
+          existinguserbyemail.verifyCode=verifycode;
+          existinguserbyemail.verifyCodeExpiry=new Date(Date.now()+ 3600000)
+          await existinguserbyemail.save()
+        }
     } else {
       //first time user
       const hashedpassword = await bcrypt.hash(password, 10);
